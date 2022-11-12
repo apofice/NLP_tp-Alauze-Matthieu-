ebe7b394ac2b3bfd760b34fe812ad4ad2f738da8
apofice <98173856+apofice@users.noreply.github.com>
2022-11-12 14:42:23 +0100

    Update jalon3.py

--- jalon3.py
+++ jalon3.py
@@ -23,7 +23,7 @@ monlabel = "Quel texte analyser ? "
 options = pd.DataFrame(['Avis dataset', 'Texte libre'])
 
 st.number_input(label= "Le nombre de topics", min_value=0, max_value=15)
-"""
+
 if st.button(label = "Détecter le sujet d'insatisfaction") == True:
     prediction(model, vectorizer, n_topics, text)
 
@@ -31,6 +31,6 @@ if st.button(label = "Détecter le sujet d'insatisfaction") == True:
 with st.sidebar:
         st.radio(monlabel, options)
         st.text_input(label="Donnez nous votre avis")
-"""
+
 #!streamlit run C:\matthieu\I3\tpNLP\jalon3.py
 
